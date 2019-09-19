5b987154cab68745ea8c555b2ec4b3c58ee0471f
unknown <krntranslation@gmail.com>
2019-09-19 15:58:29 -0500

    changes to search results

--- app.js
+++ app.js
@@ -1,112 +1,98 @@
+
 "use strict"
 /*
 Build all of your functions for displaying and gathering information below (GUI).
 */
-
 // app is the function called to start the entire application
 function app(people){
-  let searchType = prompt("Do you know the name of the person you are looking for? Enter 'yes' or 'no'").toLowerCase();
-  let searchResults;
-  switch(searchType){
-    case 'yes':
-      searchResults = searchByName(people);
-      break;
-    case 'no':
-      // TODO: search by traits
-      break;
-      default:
-    app(people); // restart app
-      break;
-  }
-  
-  // Call the mainMenu function ONLY after you find the SINGLE person you are looking for
-  mainMenu(searchResults, people);
+ let searchType = prompt("Do you know the name of the person you are looking for? Enter 'yes' or 'no'").toLowerCase();
+ let searchResults;
+ switch(searchType){
+   case 'yes':
+     searchResults = searchByName(people);
+     break;
+   case 'no':
+     prompt("Search By Trait; Enter 'Gender', 'Eye Color','Occupation'")
+     break;
+     default:
+   app(people); // restart app
+     break;
+ }
+ // Call the mainMenu function ONLY after you find the SINGLE person you are looking for
+ mainMenu(searchResults, people);
 }
-
 // Menu function to call once you find who you are looking for
 function mainMenu(person, people){
-
-  /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
-
-  if(!person){
-    alert("Could not find that individual.");
-    return app(people); // restart
-  }
-
-  let displayOption = prompt("Found " + person.firstName + " " + person.lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
-
-  switch(displayOption){
-    case "info":
-    // TODO: get person's info
-    break;
-    case "family":
-    // TODO: get person's family
-    break;
-    case "descendants":
-    // TODO: get person's descendants
-    break;
-    case "restart":
-    app(people); // restart
-    break;
-    case "quit":
-    return; // stop execution
-    default:
-    return mainMenu(person, people); // ask again
-  }
+ /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
+ if(!person){
+   alert("Could not find that individual.");
+   return app(people); // restart
+ }
+ let displayOption = prompt("Found " + person[0].firstName + " " + person[0].lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
+ switch(displayOption){
+   case "info":
+   // TODO: get person's info
+   break;
+   case "family":
+   // TODO: get person's family
+   break;
+   case "descendants":
+   // TODO: get person's descendants
+   break;
+   case "restart":
+   app(people); // restart
+   break;
+   case "quit":
+   return; // stop execution
+   default:
+   return mainMenu(person, people); // ask again
+ }
 }
-
 function searchByName(people){
-  let firstName = prompt("What is the person's first name?");
-  let lastName = prompt("What is the person's last name?");
-
-  let foundPerson = people.filter(function(person){
-    if(person.firstName === firstName && person.lastName === lastName){
-      return true;
-    }
-    else{
-      return false;
-    }
-  })
-  // TODO: find the person using the name they entered
-  return foundPerson;
+ let firstName = prompt("What is the person's first name?");
+ let lastName = prompt("What is the person's last name?");
+ let foundPerson = people.filter(function(person){
+   if(person.firstName === firstName && person.lastName === lastName){
+     return true;
+   }
+   else{
+     return false;
+   }
+ })
+ // TODO: find the person using the name they entered
+ return foundPerson;
 }
-
 // alerts a list of people
 function displayPeople(people){
-  alert(people.map(function(person){
-    return person.firstName + " " + person.lastName;
-  }).join("\n"));
+ alert(people.map(function(person){
+   return person.firstName + " " + person.lastName;
+ }).join("\n"));
 }
-
 function displayPerson(person){
-  // print all of the information about a person:
-  // height, weight, age, name, occupation, eye color.
-  let personInfo = "First Name: " + person.firstName + "\n";
-  personInfo += "Last Name: " + person.lastName + "\n";
-  // TODO: finish getting the rest of the information to display
-  alert(personInfo);
+ // print all of the information about a person:
+ // height, weight, age, name, occupation, eye color.
+ let personInfo = "First Name: " + person.firstName + "\n";
+ personInfo += "Last Name: " + person.lastName + "\n";
+ // TODO: finish getting the rest of the information to display
+ alert(personInfo);
 }
-
 // function that prompts and validates user input
 function promptFor(question, valid){
-  do{
-    let response = prompt(question).trim();
-  } while(!response || !valid(response));
-  return response;
+ do{
+   let response = prompt(question).trim();
+ } while(!response || !valid(response));
+ return response;
 }
-
 // helper function to pass into promptFor to validate yes/no answers
 function yesNo(input){
-  if (input = yes)
-  {
-    input.toLowerCase () == "yes";
-
-  }else 
-  input.toLowerCase () == "no";
-
-  return input; 
+ if (input = yes)
+ {
+   input.toLowerCase () == "yes";
+ }else
+ input.toLowerCase () == "no";
+ return input;
 }
 // helper function to pass in as default promptFor validation
 function chars(input){
-  return true; // default validation only
+ return true; // default validation only
 }
