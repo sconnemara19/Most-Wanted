4961333462bf109ae5dbc76517a969a23f39799e
unknown <krntranslation@gmail.com>
2019-09-20 16:50:08 -0500

    added 2-5 search criteria

--- app.js
+++ app.js
@@ -5,14 +5,17 @@ Build all of your functions for displaying and gathering information below (GUI)
 */
 // app is the function called to start the entire application
 function app(people){
- let searchType = prompt("Do you know the name of the person you are looking for? Enter 'yes' or 'no'").toLowerCase();
+ 
+ let searchType = prompt("Do you know the ID of the person you are looking for? Enter 'yes' or 'no'").toLowerCase();
  let searchResults;
  switch(searchType){
    case 'yes':
-     searchResults = searchByName(people);
+   searchResults = searchById(people);
      break;
    case 'no':
-     prompt("Search By Trait; Enter 'Gender', 'Eye Color','Occupation'")
+   searchResults = searchTraits(people);
+    // searchbyTrait(people)
+     //prompt("Search By Trait; Enter 'Gender', 'Eye Color','Occupation'")
      break;
      default:
    app(people); // restart app
@@ -23,52 +26,92 @@ function app(people){
 }
 // Menu function to call once you find who you are looking for
 function mainMenu(person, people){
+
+
  /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
  if(!person){
    alert("Could not find that individual.");
    return app(people); // restart
+
  }
- let displayOption = prompt("Found " + person[0].firstName + " " + person[0].lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
+  if (person[0].parents.length == 0) 
+    {
+      person[0].parents = "Does not have parents";
+    }
+
+  if (person[0].currentSpouse == null) 
+    {
+      person[0].currentSpouse = "Does not have a spouse";
+    }
+
+
+
+if (person.length <= 1) {
+ let displayOption = prompt("Found " + person[0].firstName + " " + person[0].lastName + " Select from the options below:  'Gender', 'DOB, 'Height', 'Weight', 'Eyecolor', 'Occupation', 'Parents', 'Current Spouse', 'Info' (display all), or 'restart' or 'quit'");
+    
+
  switch(displayOption){
    case "info": console.log(person[0]);
    // TODO: get person's info
    break;
-   case "family": console.log(person[0].currentSpouse);
+   case "Current Spouse": console.log(person[0].currentSpouse);
    // TODO: get person's family
    break;
-   case "descendants": 
-
-  //  for (i = 0; i <= people.length; i++)
-  // {
-  //  if (people[i].parents == person[0].id)
-  //  {
-  //    console.log(people[i].name)
-  //  }
-  // }
-   // TODO: get person's descendants
+   case "gender": console.log(person[0].gender);
+   // TODO: get person's info
+   break;
+   case "dob": console.log(person[0].dob);
+   // TODO: get person's family
+   break;
+   case "height": console.log(person[0].height);
+   // TODO: get person's info
+   break;
+   case "weight": console.log(person[0].weight);
+   // TODO: get person's family
+   break;
+    case "eyecolor": console.log(person[0].eyeColor);
+   // TODO: get person's info
+   break;
+   case "occupation": console.log(person[0].occupation);
+   // TODO: get person's family
+   break;
+    case "parents": console.log(person[0].parents);
+   // TODO: get person's info
    break;
    case "restart":
    app(people); // restart
    break;
    case "quit":
    return; // stop execution
-   default:
+   default: console.log("Item cannot be found");
+
    return mainMenu(person, people); // ask again
- }
+ 
+    }
+
+  }
+  else {
+    displayPeople(person);
+      }
+        let results = searchTraits(person);
+mainMenu(results);
+
 }
-function searchByName(people){
- let firstName = prompt("What is the person's first name?");
- let lastName = prompt("What is the person's last name?");
- let foundPerson = people.filter(function(person){
-   if(person.firstName === firstName && person.lastName === lastName){
+function searchById(people){
+let idVariable = parseInt(prompt("PLease Enter the ID#"));
+
+ let foundId = people.filter(function(person){
+   if(person.id === idVariable){
      return true;
    }
    else{
      return false;
    }
  })
+
+
  // TODO: find the person using the name they entered
- return foundPerson;
+ return foundId;
 }
 // alerts a list of people
 function displayPeople(people){
@@ -76,6 +119,7 @@ function displayPeople(people){
    return person.firstName + " " + person.lastName;
  }).join("\n"));
 }
+
 function displayPerson(person){
  // print all of the information about a person:
  // height, weight, age, name, occupation, eye color.
@@ -104,3 +148,64 @@ function yesNo(input){
 function chars(input){
  return true; // default validation only
 }
+
+
+function searchTraits(people){
+ let userInput = prompt("Do you want to by individual traits? Type in a trait. 'Gender', 'Age', 'Height', 'Weight', 'Eye color', 'Occupation'.")
+
+let foundTrait = people.filter(function(el){
+   if(el.gender === userInput){
+     return true;
+    }
+    else if(el.eyeColor === userInput){
+      return true;
+    }
+    else if(el.occupation === userInput){
+      return true;
+    }
+    else if(el.weight === userInput){
+      return true
+    }
+    else if(el.height === userInput){
+      return true;
+    }
+    else{
+      return false;
+    }
+
+
+ });
+
+return foundTrait;
+}
+//let genderTrait = 
+//let weightTrait = people.filter(function(person){
+  // if(person.id === idVariable){
+     //return true; 
+//let heightTrait = people.filter(function(person){
+ //  if(person.id === idVariable){
+    // return true;
+
+//})
+//let occupationTrait = people.filter(function(person){
+  // if(person.id === idVariable){
+    // return true;
+
+//})){
+  // if(person.id === idVariable){
+    // return true;
+
+//})
+
+  //return true;
+
+ //let foundId = 
+//console.log(filterTrait);
+
+
+
+ 
+
+
+
+
