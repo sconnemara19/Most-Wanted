83f03f0e25d1dbaf8f859b688b27292a392346ea
Jacob Carson <jaycar641@yahoo.com>
2019-09-20 10:40:01 -0500

    Finished Single Criterion

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ Build all of your functions for displaying and gathering information below (GUI)
 */
 // app is the function called to start the entire application
 function app(people){
- let searchType = prompt("Do you know the name of the person you are looking for? Enter 'yes' or 'no'").toLowerCase();
+ let searchType = prompt("Do you know the ID of the person you are looking for? Enter 'yes' or 'no'").toLowerCase();
  let searchResults;
  switch(searchType){
    case 'yes':
-     searchResults = searchByName(people);
+   searchResults = searchById(people);
      break;
    case 'no':
-     prompt("Search By Trait; Enter 'Gender', 'Eye Color','Occupation'")
+    // searchbyTrait(people)
+     //prompt("Search By Trait; Enter 'Gender', 'Eye Color','Occupation'")
      break;
      default:
    app(people); // restart app
@@ -23,52 +24,85 @@ function app(people){
 }
 // Menu function to call once you find who you are looking for
 function mainMenu(person, people){
+
+
  /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
  if(!person){
    alert("Could not find that individual.");
    return app(people); // restart
+
  }
- let displayOption = prompt("Found " + person[0].firstName + " " + person[0].lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
+  if (person[0].parents.length == 0) 
+    {
+      person[0].parents = "Does not have parents";
+    }
+
+  if (person[0].currentSpouse == null) 
+    {
+      person[0].currentSpouse = "Does not have a spouse";
+    }
+
+
+
+
+ let displayOption = prompt("Found " + person[0].firstName + " " + person[0].lastName + " Select from the options below:  'Gender', 'DOB, 'Height', 'Weight', 'Eyecolor', 'Occupation', 'Parents', 'Current Spouse', 'Info' (display all), or 'restart' or 'quit'");
+  
+
  switch(displayOption){
    case "info": console.log(person[0]);
    // TODO: get person's info
    break;
-   case "family": console.log(person[0].currentSpouse);
+   case "Current Spouse": console.log(person[0].currentSpouse);
    // TODO: get person's family
    break;
-   case "descendants": 
-
-  //  for (i = 0; i <= people.length; i++)
-  // {
-  //  if (people[i].parents == person[0].id)
-  //  {
-  //    console.log(people[i].name)
-  //  }
-  // }
-   // TODO: get person's descendants
+   case "gender": console.log(person[0].gender);
+   // TODO: get person's info
+   break;
+   case "dob": console.log(person[0].dob);
+   // TODO: get person's family
+   break;
+   case "height": console.log(person[0].height);
+   // TODO: get person's info
+   break;
+   case "weight": console.log(person[0].weight);
+   // TODO: get person's family
+   break;
+    case "eyecolor": console.log(person[0].eyeColor);
+   // TODO: get person's info
+   break;
+   case "occupation": console.log(person[0].occupation);
+   // TODO: get person's family
+   break;
+    case "parents": console.log(person[0].parents);
+   // TODO: get person's info
    break;
    case "restart":
    app(people); // restart
    break;
    case "quit":
    return; // stop execution
-   default:
+   default: console.log("Item cannot be found");
+
    return mainMenu(person, people); // ask again
- }
+ 
+    }
+
 }
-function searchByName(people){
- let firstName = prompt("What is the person's first name?");
- let lastName = prompt("What is the person's last name?");
- let foundPerson = people.filter(function(person){
-   if(person.firstName === firstName && person.lastName === lastName){
+function searchById(people){
+let idVariable = parseInt(prompt("PLease Enter the ID#"));
+
+ let foundId = people.filter(function(person){
+   if(person.id === idVariable){
      return true;
    }
    else{
      return false;
    }
  })
+
+
  // TODO: find the person using the name they entered
- return foundPerson;
+ return foundId;
 }
 // alerts a list of people
 function displayPeople(people){
